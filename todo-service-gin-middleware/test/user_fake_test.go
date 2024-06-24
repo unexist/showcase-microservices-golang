539a4d2155aa527286913b82d7bacd736ff68e84
Christoph Kappel <christoph@unexist.dev>
2024-06-24 10:10:42 +0200

    Fixed user tests

--- todo-service-gin-middleware/test/user_fake_test.go
+++ todo-service-gin-middleware/test/user_fake_test.go
@@ -1,7 +1,7 @@
 //
 // @package Showcase-Microservices-Golang
 //
-// @file Todo tests for fake repository
+// @file User tests for fake repository
 // @copyright 2023-present Christoph Kappel <christoph@unexist.dev>
 // @version $Id$
 //
@@ -9,69 +9,33 @@
 // See the file LICENSE for details.
 //
 
-//go:build fake
-
 package test
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/stretchr/testify/assert"
-	"github.com/unexist/showcase-microservices-golang/infrastructure"
-
-	"os"
+	"fmt"
 	"testing"
 
-	"bytes"
-	"github.com/unexist/showcase-microservices-golang/adapter"
-	"github.com/unexist/showcase-microservices-golang/domain"
 	"net/http"
-	"net/http/httptest"
 )
-
-/* Test globals */
-var (
-	engine         *gin.Engine
-	todoRepository *infrastructure.TodoFakeRepository
-	userRepository *infrastructure.UserFakeRepository
-)
-
-func TestMain(m *testing.M) {
-	/* Create business stuff */
-	todoRepository = infrastructure.NewTodoFakeRepository()
-	userRepository = infrastructure.NewUserFakeRepository()
-	todoService := domain.NewTodoService(todoRepository)
-	userService := domain.NewUserService(userRepository)
-	todoResource := adapter.NewTodoResource(todoService)
-	userResource := adapter.NewUserResource(userService)
-
-	/* Finally start Gin */
-	engine = gin.Default()
-
-	todoResource.RegisterRoutes(engine)
-	userResource.RegisterRoutes(engine)
-
-	code := m.Run()
-
-	os.Exit(code)
-}
-
-func executeRequest(req *http.Request) *httptest.ResponseRecorder {
-	recorder := httptest.NewRecorder()
-
-	engine.ServeHTTP(recorder, req)
-
-	return recorder
-}
-
-func checkResponseCode(t *testing.T, expected, actual int) {
-	assert.Equal(t, expected, actual, "Expected different response code")
-}
 
 func TestLogin(t *testing.T) {
 	todoRepository.Clear()
+	userRepository.Close()
 
-	req, _ := http.NewRequest("POST", "/user/login", bytes.NewBuffer(jsonStr))
+	req, _ := http.NewRequest("POST", "/user/login", nil)
 	req.Header.Set("Content-Type", "application/json")
+
+	response := executeRequest(req)
+	checkResponseCode(t, http.StatusOK, response.Code)
+}
+
+func TestSelf(t *testing.T) {
+	todoRepository.Clear()
+	userRepository.Close()
+
+	req, _ := http.NewRequest("GET", "/user/self", nil)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", executeLogin(t)))
 
 	response := executeRequest(req)
 	checkResponseCode(t, http.StatusOK, response.Code)
