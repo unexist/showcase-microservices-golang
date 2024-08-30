2c9f6bf194ed860128a9e7574c90f8a1f16d2f92
Christoph Kappel <christoph@unexist.dev>
2024-08-30 14:46:36 +0200

    Added gin-metrics

--- todo-service-gin-metrics/main.go
+++ todo-service-gin-metrics/main.go
@@ -12,20 +12,10 @@
 package main
 
 import (
-	"context"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
-	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
-	"go.opentelemetry.io/otel/exporters/zipkin"
-	"go.opentelemetry.io/otel/propagation"
-	"go.opentelemetry.io/otel/sdk/resource"
-	"google.golang.org/grpc/encoding/gzip"
-
-	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	"github.com/penglongli/gin-metrics/ginmetrics"
 
 	"github.com/unexist/showcase-microservices-golang/adapter"
 	"github.com/unexist/showcase-microservices-golang/domain"
@@ -37,16 +27,6 @@ import (
 )
 
 func main() {
-	/* Init tracer */
-	ctx := context.Background()
-
-	provider := initTracer(ctx)
-	defer func() {
-		if err := provider.Shutdown(context.Background()); nil != err {
-			log.Printf("Error shutting down tracer provider: %v", err)
-		}
-	}()
-
 	/* Create business stuff */
 	var todoRepository *infrastructure.TodoGormRepository
 
@@ -73,60 +53,15 @@ func main() {
 	/* Finally start Gin */
 	engine := gin.Default()
 
-	engine.Use(otelgin.Middleware("todo-service"))
+	/* Create monitor */
+	monitor := ginmetrics.GetMonitor()
+
+	monitor.SetMetricPath("/metrics")
+	monitor.SetSlowTime(10)
+	monitor.SetDuration([]float64{0.1, 0.3, 1.2, 5, 10})
+	monitor.Use(engine)
 
 	todoResource.RegisterRoutes(engine)
 
 	log.Fatal(http.ListenAndServe(":8080", engine))
-}
-
-func initTracer(ctx context.Context) *sdktrace.TracerProvider {
-	var exporter sdktrace.SpanExporter
-	var err error
-
-	/* Create trace exporter */
-	if "jaeger" == os.Getenv("TRACER") {
-		exporter, err = otlptracegrpc.New(ctx,
-			otlptracegrpc.WithEndpoint("localhost:14268"),
-			otlptracegrpc.WithCompressor(gzip.Name),
-		)
-	} else {
-		exporter, err = zipkin.New("http://localhost:9411/api/v2/spans",
-			zipkin.WithLogger(log.New(os.Stderr, "todo-service",
-				log.Ldate|log.Ltime|log.Llongfile)),
-		)
-	}
-
-	if nil != err {
-		log.Fatal(err)
-	}
-
-	/* Create processor */
-	batcher := sdktrace.NewBatchSpanProcessor(exporter,
-		sdktrace.WithMaxQueueSize(1000),
-		sdktrace.WithMaxExportBatchSize(1000))
-
-	/* Create resource */
-	resource, err := resource.New(ctx,
-		resource.WithFromEnv(),
-		resource.WithTelemetrySDK(),
-		resource.WithHost(),
-		resource.WithAttributes(
-			attribute.String("service.name", "todo-service"),
-			attribute.String("service.version", "1.0.0"),
-		))
-	if nil != err {
-		log.Fatal(err)
-	}
-
-	provider := sdktrace.NewTracerProvider(
-		sdktrace.WithResource(resource),
-		sdktrace.WithSampler(sdktrace.AlwaysSample()),
-		sdktrace.WithSpanProcessor(batcher),
-	)
-	otel.SetTracerProvider(provider)
-	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(
-		propagation.TraceContext{}, propagation.Baggage{}))
-
-	return provider
 }
