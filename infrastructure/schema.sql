c2cb81f0fcbe9af6f7a202ec9193b93c0248067d
Christoph Kappel <christoph@unexist.dev>
2024-09-19 12:05:10 +0200

    Replaced serial type with identity

--- infrastructure/schema.sql
+++ infrastructure/schema.sql
@@ -1,8 +1,9 @@
 CREATE TABLE todos
 (
-    id SERIAL,
+    -- https://www.naiyerasif.com/post/2024/09/04/stop-using-serial-in-postgres/
+    --id SERIAL PRIMARY KEY,
+    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     uuid TEXT NOT NULL,
     title TEXT NOT NULL,
-    description TEXT NOT NULL,
-    CONSTRAINT todos_pkey PRIMARY KEY (id)
-)
+    description TEXT NOT NULL
+);
