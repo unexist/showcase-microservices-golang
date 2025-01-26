ab0f0551f0580e593ab827504b912c5e250c7bb3
Christoph Kappel <christoph@unexist.dev>
2025-01-26 12:23:07 +0100

    Updated schema files

--- infrastructure/users.sql
+++ infrastructure/users.sql
@@ -1,9 +1,8 @@
-CREATE TABLE todos
+CREATE TABLE users
 (
     -- https://www.naiyerasif.com/post/2024/09/04/stop-using-serial-in-postgres/
     --id SERIAL PRIMARY KEY,
     id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    uuid TEXT NOT NULL,
-    title TEXT NOT NULL,
-    description TEXT NOT NULL
+    name TEXT NOT NULL,
+    token TEXT NOT NULL
 );
